7d110c289efdc7309ffe9d917f582515cffd4e88
Adarsh <adarsh9540984202@gmail.com>
2024-05-13 12:56:07 +0530

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 import HeroSection from "@/components/HeroSection";
 import Footer from "@/components/Footer";
 import DrawerAppBar from "@/components/Navbar";
+import Body from "@/components/Body";
+
 
 export default function Home() {
   return (
     <div>
       <DrawerAppBar/>
       <HeroSection/>
-      <Footer/>
+      <Body/>
     </div>
 
   );
